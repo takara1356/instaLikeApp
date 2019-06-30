eba37ddf79c9b53bdcad49b7e03eaf057372f1d1
takara1356 <goblin7283@gmail.com>
2019-06-30 16:23:53 +0900

    Postモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_29_084814) do
+ActiveRecord::Schema.define(version: 2019_06_30_071939) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string "content"
+    t.string "image"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
